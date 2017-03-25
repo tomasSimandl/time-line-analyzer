0ca20d0b635bfe9d24c39b3aab24d907e2b5900b
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-03-25 10:17:02 +0100

    add functions for data loading

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -2,38 +2,159 @@ library(shiny)
 library(anytime)
 library(data.table)
 library(plotly)
+# =================================================================================================================================
+# ========================================================= DATA READING ==========================================================
+# =================================================================================================================================
 
-# load csv and take value by 1 second
-load_csv <- function(file) {
+# decide which method use for read data ===========================================================================================
+load_data <- function(file, deviceSelect, startTime, endTime, timeInterval, timeShift){
+   switch(deviceSelect,
+      chest_strap = load_chest_strap_csv(file, startTime, endTime, timeInterval, timeShift),
+      garmin = load_garmin_tcx(file, startTime, endTime, timeInterval, timeShift),
+      basis = load_basis_csv(file, startTime, endTime, timeInterval, timeShift),
+      fitbit = load_fitbit_csv(file, startTime, endTime, timeInterval, timeShift),
+      return(NULL))
+   
+} # ==============================================================================================================================
+
+load_garmin_tcx <- function(file, startTime, endTime, timeInterval, timeShift){
    if(is.null(file)) return(NULL)
-   data <- read.csv(file, header = FALSE)
-   specificColumns <- data.table(time = floor(data$V1/1000), bpm = data$V3)
-   means = specificColumns[, mean(bpm), by=time]
+   
+   soubor <- xmlParse(file)
+   data <- xmlToDataFrame(getNodeSet(soubor, "//ns:Trackpoint", "ns"))
+   formated_data <- data.table(time = as.numeric(format(strptime(x = data$Time, format = "%Y-%m-%dT%H:%M:%S"), format = "%s")) + timeShift, bpm = as.numeric(as.character(data$HeartRateBpm)))
+   
+   data_sampling(formated_data, get_time(startTime), get_time(endTime), timeInterval)
 }
 
+load_basis_csv <- function(file, startTime, endTime, timeInterval, timeShift){
+   columnsNames <- c("", "time", "", "", "bpm", "", "")
+   
+   data <- load_csv(file, columnsNames, header = TRUE)
+   formated_data <- data.table(time = as.numeric(format(as.POSIXct(data$time), format = "%s"))  + timeShift, bpm = data$bpm)
+ 
+   data_sampling(formated_data, get_time(startTime), get_time(endTime), timeInterval)  
+}
+
+load_fitbit_csv <- function(file, startTime, endTime, timeInterval, timeShift){
+   columnsNames <- c("date", "time", "bpm")
+   
+   data <- load_csv(file, columnsNames, header = FALSE)
+   formated_data <- data.table(time = as.numeric(format(as.POSIXct(paste(data$date, data$time, sep = " ")), format = "%s"))  + timeShift, bpm = data$bpm)
+   
+   data_sampling(formated_data, get_time(startTime), get_time(endTime), timeInterval)  
+}
+
+# load data from csv for chest strap and create sampling =========================================================================
+load_chest_strap_csv <- function(file, startTime, endTime, timeInterval, timeShift) {
+   columnsNames <- c("time", "", "bpm", "", "", "", "", "", "")
+   
+   data <- load_csv(file, columnsNames, header = FALSE)
+   data_by_sec <- data.table(time = floor(data$time/1000) + timeShift, bpm = data$bpm)[, mean(bpm), by=time]
+   end <- data.table(time = data_by_sec$time, bpm = data_by_sec$V1) # TODO
+   
+   data_sampling(end, get_time(startTime), get_time(endTime), timeInterval)
+} # ==============================================================================================================================
+
+# load data from csv with specific names of columns ==============================================================================
+load_csv <- function(file, columnsNames, header = FALSE) {
+   if(is.null(file)) return(NULL)
+   
+   read.csv(file, header = header, col.names = columnsNames)
+} # ==============================================================================================================================
+
+
+
+
+
+
+
+
+
+
+# calculate average for data by timeInterval. ====================================================================================
+data_sampling <- function(data, startTime, endTime, timeInterval){
+   sum <- 0
+   counter <- 0
+   sumStartTime <- startTime
+   i <- 1L
+   sequence <- seq(startTime, endTime, timeInterval)
+   DT <- data.table(time = sequence, bpm = numeric(length(sequence)))
+
+   for (n in 1:nrow(data)){
+      curTime <- data[n]$time
+      print(data[n]$bpm)
+      if(curTime < startTime) next
+      if(curTime > endTime) break
+      if(curTime == startTime){
+         set(DT, i, "bpm", data[n]$bpm)
+         i <- i + 1L
+         next
+      }
+      
+      if(sumStartTime + timeInterval < curTime){
+         sumStartTime <- sumStartTime + timeInterval
+         if (counter != 0){
+            set(DT, i, "bpm", sum/counter)
+         }
+         
+         counter <- 0
+         sum <- 0
+         i <- i + 1L
+      }
+      while (sumStartTime + timeInterval < curTime){
+         sumStartTime <- sumStartTime + timeInterval
+         i <- i + 1L
+      }
+      
+      sum <- sum + data[n]$bpm
+      counter <- counter + 1
+   }
+   if (counter != 0 && sumStartTime + timeInterval <= endTime){
+      set(DT, i, "bpm", sum/counter)
+   }
+   DT
+} # ==============================================================================================================================
+
+
+calculate_sd <- function(time_line1, time_line2){
+   
+}
+
+
+get_time <- function(strTime) {
+   as.numeric(format(strptime(x = strTime, format = "%d.%m.%Y %H:%M:%S"), format = "%s"))
+}
+
+# =================================================================================================================================
+# ============================================================ GRAPHS =============================================================
+# =================================================================================================================================
+
 # create plot from two input time lines
-create_plot <- function(time_line1, time_line2){
-   if (is.null(time_line1) || is.null(time_line2)) return(NULL)
+create_plot <- function(time_line1, time_line2, name1, name2){
+   validate(need(!is.null(time_line1) && !is.null(time_line2),'Can not create a plot. No input data.'))
    
-   max <- max(c(max(time_line1$V1), max(time_line2$V1)))
-   min <- min(c(min(time_line1$V1), min(time_line2$V1)))
+   max <- max(c(max(time_line1$bpm), max(time_line2$bpm)))
+   min <- min(c(min(time_line1$bpm), min(time_line2$bpm)))
    
-   plot_ly(y = ~time_line1$V1, name = 'trace 0', type = 'scatter', mode = 'lines')%>%
-      add_trace(y = ~time_line2$V1, name = 'trace 1', mode = 'lines')
+   plot_ly(y = time_line1$bpm, x = anytime(time_line1$time), name = name1, type = 'scatter', mode = 'lines')%>%
+      add_trace(y = time_line2$bpm, name = name2, mode = 'lines')%>%
+      layout(xaxis = list(title = "Time"), yaxis = list(title = "BPM"))
 }
 
 # create plot of residuas from two input time lines
-create_resi_plot <- function(time_line1, time_line2){
-   if (is.null(time_line1) || is.null(time_line2)) return(NULL)
+create_resi_plot <- function(residua){
+   validate(need(!is.null(residua),'Can not create a plot. No input data.'))
    
-   residua <- data.table(time = time_line1$time, bpm = time_line1$V1 - time_line2$V1)
-   
-   plot_ly(residua, y = ~bpm, name = 'trace 0', type = 'scatter', mode = 'lines')
+   plot_ly(residua, y = ~bpm, x = anytime(residua$time), name = 'trace 0', type = 'scatter', mode = 'lines')%>%
+      layout(xaxis = list(title = "Time"), yaxis = list(title = "BPM"))
 }
 
 
 
-
+# =================================================================================================================================
+# ============================================================ SERVER =============================================================
+# =================================================================================================================================
 function(input, output, session) {
    
    # observe submit button to switch on next panel in navbar
@@ -41,52 +162,74 @@ function(input, output, session) {
       if(is.null(inputLow1()) || is.null(inputLow2()) || is.null(inputMed1()) || is.null(inputMed2()) || is.null(inputHig1()) || is.null(inputHig2())){
          return(NULL)
       }
-      updateNavbarPage(session, "navbar", selected = "tabPlot")
+      updateNavbarPage(session, "navbar", selected = "tabSummary")
    })
    
    
    # load files when change associated fileInput
    inputLow1 <- reactive({
-      load_csv(input$inFileLow1$datapath)
+      load_data(file = input$inFileLow1$datapath, deviceSelect = input$deviceSelect1, input$startMeasLow, input$endMeasLow, input$timeIntervalInput,  input$timeZone1*3600)
    })
    inputMed1 <- reactive({
-      load_csv(input$inFileMed1$datapath)
+      load_data(file = input$inFileMed1$datapath, deviceSelect = input$deviceSelect1, input$startMeasMed, input$endMeasMed, input$timeIntervalInput,  input$timeZone1*3600)
    })
    inputHig1 <- reactive({
-      load_csv(input$inFileHig1$datapath)
+      load_data(file = input$inFileHig1$datapath, deviceSelect = input$deviceSelect1, input$startMeasHig, input$endMeasHig, input$timeIntervalInput,  input$timeZone1*3600)
    })
    inputLow2 <- reactive({
-      load_csv(input$inFileLow2$datapath)
+      load_data(file = input$inFileLow2$datapath, deviceSelect = input$deviceSelect2, input$startMeasLow, input$endMeasLow, input$timeIntervalInput, input$timeShiftLow + (input$timeZone2*3600))
    })
    inputMed2 <- reactive({
-      load_csv(input$inFileMed2$datapath)
+      load_data(file = input$inFileMed2$datapath, deviceSelect = input$deviceSelect2, input$startMeasMed, input$endMeasMed, input$timeIntervalInput, input$timeShiftMed + (input$timeZone2*3600))
    })
    inputHig2 <- reactive({
-      load_csv(input$inFileHig2$datapath)
+      load_data(file = input$inFileHig2$datapath, deviceSelect = input$deviceSelect2, input$startMeasHig, input$endMeasHig, input$timeIntervalInput, input$timeShiftHig + (input$timeZone2*3600))
+   })
+   
+   residuaLow <- reactive({
+      time_line1 <- inputLow1
+      time_line2 <- inputLow2
+      if(is.null(time_line1) || is.null(time_line2)) return(NULL)
+      
+      data.table(time = time_line1$time, bpm = time_line1$bpm - time_line2$bpm)
+   })
+   residuaMid <- reactive({
+      time_line1 <- inputMid1
+      time_line2 <- inputMid2
+      if(is.null(time_line1) || is.null(time_line2)) return(NULL)
+      
+      data.table(time = time_line1$time, bpm = time_line1$bpm - time_line2$bpm)
+   })
+   residuaHig <- reactive({
+      time_line1 <- inputHig1
+      time_line2 <- inputHig2
+      if(is.null(time_line1) || is.null(time_line2)) return(NULL)
+      
+      data.table(time = time_line1$time, bpm = time_line1$bpm - time_line2$bpm)
    })
    
    
    # render plots with two lines
    output$plotLow <- renderPlotly({
-      create_plot(inputLow1(), inputLow2())
+      create_plot(inputLow1(), inputLow2(), input$deviceSelect1, input$deviceSelect2)
    })
    output$plotMed <- renderPlotly({
-      create_plot(inputMed1(), inputMed2())
+      create_plot(inputMed1(), inputMed2(), input$deviceSelect1, input$deviceSelect2)
    })
    output$plotHig <- renderPlotly({
-      create_plot(inputHig1(), inputHig2())
+      create_plot(inputHig1(), inputHig2(), input$deviceSelect1, input$deviceSelect2)
    })
    
    
    # render plots with residuas
    output$plotResiLow <- renderPlotly({
-      create_resi_plot(inputLow1(), inputLow2())
+      create_resi_plot(residuaLow())
    })
    output$plotResiMed <- renderPlotly({
-      create_resi_plot(inputMed1(), inputMed2())
+      create_resi_plot(residuaMid())
    })
    output$plotResiHig <- renderPlotly({
-      create_resi_plot(inputHig1(), inputHig2())
+      create_resi_plot(residuaHig())
    })
    
    
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -1,14 +1,19 @@
 library(shiny)
 library(shinythemes)
 library(plotly)
+library(shinyTime)
 
 shinyUI(
-  navbarPage("Data Analyzer", id = "navbar", theme = shinytheme("yeti"),
+  navbarPage("Data Analyzer", id = "navbar", theme = shinytheme("darkly"),
              
     # Panel for file input
     tabPanel("File input", value = "tabFileInput",
       wellPanel(
         h2("First data set"),
+        fluidRow(
+           column(width = 6, selectInput(inputId = "deviceSelect1", label = "Choose a device", c("Chest strap" = "chest_strap", "Garmin" = "garmin", "Basis" = "basis", "Fitbit" = "fitbit"))),
+           column(width = 6, numericInput(inputId = "timeZone1", label = "Time zone shift [h]", value = 0, min = -12, max = 14, step = 0.25))
+        ),
         fluidRow(
           column(width = 4,fileInput(inputId = "inFileLow1", label = "Low", multiple = FALSE)),
           column(width = 4,fileInput(inputId = "inFileMed1", label = "Medium", multiple = FALSE)),
@@ -18,50 +23,111 @@ shinyUI(
       wellPanel(
         h2("Second data set"),
         fluidRow(
+           column(width = 6, selectInput(inputId = "deviceSelect2", label = "Choose a device", c("Chest strap" = "chest_strap", "Garmin" = "garmin", "Basis" = "basis", "Fitbit" = "fitbit"))), 
+           column(width = 6, numericInput(inputId = "timeZone2", label = "Time zone shift [h]", value = 0, min = -12, max = 14, step = 0.25))
+        ),
+        fluidRow(
           column(width = 4,fileInput(inputId = "inFileLow2", label = "Low", multiple = FALSE)),
           column(width = 4,fileInput(inputId = "inFileMed2", label = "Medium", multiple = FALSE)),
           column(width = 4,fileInput(inputId = "inFileHig2", label = "High", multiple = FALSE))
         )
       ),
+      wellPanel(
+         h2("Setting"),
+         fluidRow(
+            column(width = 4, h4("Measurement interval for low load")),
+            column(width = 4, h4("Measurement interval for medium load")),
+            column(width = 4, h4("Measurement interval for high load"))
+         ),
+         fluidRow(
+            column(width = 2, textInput(inputId = "startMeasLow", "From", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S"))),
+            column(width = 2, textInput(inputId = "endMeasLow", "To", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S"))),
+            column(width = 2, textInput(inputId = "startMeasMed", "From", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S"))),
+            column(width = 2, textInput(inputId = "endMeasMed", "To", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S"))),
+            column(width = 2, textInput(inputId = "startMeasHig", "From", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S"))),
+            column(width = 2, textInput(inputId = "endMeasHig", "To", value = format(Sys.time(), format = "%d.%m.%Y %H:%M:%S")))
+         ),
+         fluidRow(
+            column(width = 4, numericInput(inputId = "timeShiftLow", label = "Time shift for low load [s]", value = 0, min = -1000, max = 1000)),
+            column(width = 4, numericInput(inputId = "timeShiftMed", label = "Time shift for low load [s]", value = 0, min = -1000, max = 1000)),
+            column(width = 4, numericInput(inputId = "timeShiftHig", label = "Time shift for low load [s]", value = 0, min = -1000, max = 1000))
+         ),
+         fluidRow(
+            column(width = 4, numericInput(inputId = "timeIntervalInput", label = "Choose interval for data sampling [s]", value = 1, min = 1, max = 60))
+         )
+      ),
       actionButton(inputId = "submitBtn", label = "Submit")
     ),
     
-    # panel for compare plots
-    tabPanel("Plot", value = "tabPlot",
-      wellPanel(
-         h2("Plot comparing low BPMs"),
-         plotlyOutput("plotLow", width = "100%")
-      ),
-      wellPanel(
-         h2("Plot comparing medium BPMs"),
-         plotlyOutput("plotMed", width = "100%")
-      ),
-      wellPanel(
-         h2("Plot comparing hight BPMs"),
-         plotlyOutput("plotHig", width = "100%")
-      )
+    tabPanel("LOW", value = "tabLow",
+             h1("Compare of two time lines for low load"),
+             wellPanel(
+                h2("Calculations")
+                
+             ),
+             wellPanel(
+                h2("Plot comparing low BPMs"),
+                plotlyOutput("plotLow", width = "100%")
+             ),
+             wellPanel(
+                h2("Plot of residuals for low BPMs"),
+                plotlyOutput("plotResiLow", width = "100%")
+             ),
+             wellPanel(
+                plotOutput("boxPlotLow", width = "100%")
+             ),
+             wellPanel(
+                "bland altman plot"
+             )
     ),
     
-    # panel for residuas plot
-    tabPanel("Residues plot", value = "tabResiduasPlot",
-      plotOutput("plotResiLow", width = "100%"),
-      plotOutput("plotResiMed", width = "100%"),
-      plotOutput("plotResiHig", width = "100%")
+    tabPanel("MEDIUM", value = "tabMedium",
+             h1("Compare of two time lines for medium load"),
+             wellPanel(
+                "Calculations"
+             ),
+             wellPanel(
+                h2("Plot comparing medium BPMs"),
+                plotlyOutput("plotMed", width = "100%")
+             ),
+             wellPanel(
+                h2("Plot of residuals for medium BPMs"),
+                plotlyOutput("plotResiMed", width = "100%")
+             ),
+             wellPanel(
+                plotOutput("boxPlotMed", width = "100%")
+             ),
+             wellPanel(
+                "bland altman plot"
+             )
     ),
     
-    # panel for box plost for outliers visualization
-    tabPanel("Outliers", value = "tabOutliers",
-      plotOutput("boxPlot", width = "100%")
+    tabPanel("HIGH", value = "tabHigh",
+             h1("Compare of two time lines for high load"),
+             wellPanel(
+                "Calculations"
+             ),
+             wellPanel(
+                h2("Plot comparing hight BPMs"),
+                plotlyOutput("plotHig", width = "100%")
+             ),
+             wellPanel(
+                h2("Plot of residuals for high BPMs"),
+                plotlyOutput("plotResiHig", width = "100%")
+             ),
+             wellPanel(
+                plotOutput("boxPlotHig", width = "100%")
+             ),
+             wellPanel(
+                "bland altman plot"
+             )
     ),
     
-    # panel for statistic calculation
-    tabPanel("SD, Mean, Cov", value = "tabStatisticCalc",
-      actionButton(inputId = "submitBtn3", label = "Submit")
-    ),
-    
-    # panel for Bland Altman plot
-    tabPanel("Bland&Altman plot", value = "tabBAPlot",
-      actionButton(inputId = "submitBtn2", label = "Submit")
+    tabPanel("Summary", value = "tabSummary",
+             h1("Compare of two time lines for low, medium and height load"),
+             wellPanel(
+                "Calculations"
+             )
     ),
     
     # panel for tables with input files data
